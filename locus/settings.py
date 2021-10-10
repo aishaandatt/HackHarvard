40b53501e20b38466563fd1dbf697d4d4cc6091b
aishaandatt <63548745+aishaandatt@users.noreply.github.com>
2021-10-10 19:06:00 +0530

    Update settings.py

--- locus/settings.py
+++ locus/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_faw@ynclj+jv**r$_dsrccl+z=lhc8vo&v2m&21yeq5)*n$@+'
+SECRET_KEY = 'xxxxxxxxx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
